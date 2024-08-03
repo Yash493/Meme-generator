10e0faa925be8e2b76b022e2d17ea53fc1581848
YASH <yashgaikwad493@gmail.com>
2024-08-04 00:47:17 +0530

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+      <header className="header">
+          <img 
+              src="./images/img1.png" 
+              className="header--image"
+              alt="notimg"
+          />
+          <h2 className="header--title">Meme Generator</h2>
+
+      </header>
+  )
+}
